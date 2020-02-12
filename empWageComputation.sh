376854978899f747810d9de9482e4991ef0baebd
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-12 18:00:51 +0530

    [Mayank] Add. adding the attendance check logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,14 @@
-#!bin/bash
-echo   "============================================"
-echo   "welcome To Employee Wage Computation Program"
-echo   "============================================"
+#!/bin/bash
+attendanceResult=0
+attendanceCheck()
+{
+	if(($((RANDOM%2==0))))
+    	then
+		attendanceResult=0
+		echo "Employee is absent today"
+	else
+		attendanceResult=1
+		echo "Employee is present today"
+	fi
+}
+attendanceCheck
