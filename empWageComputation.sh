ac618768706fa17a9f6443cdb53cdcf770a324a4
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-12 19:30:10 +0530

    Revert "[Mayank] Refactor. adding jobtype selection functionality" This reverts commit 94d078d9e02da59feed520a57a5f66b8033d16c1.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,36 @@
 #!bin/bash
-echo -e "Please select the options:\n1.Full time\n2.PartTime"
-read jobType
+<<<<<<< HEAD
+echo   "================================================"
+echo   "welcome To the Employee Wage Computation Program"
+echo   "================================================"
+attendanceResult=0
+attendanceCheck()
+{
+	if(($((RANDOM%2==0))))
+    	then
+		attendanceResult=0
+		echo "Employee is absent today"
+	else
+		attendanceResult=1
+		echo "Employee is present today"
+	fi
+}
+attendanceCheck
 
-case $jobType in
-    1)
-		echo "Full time"
-		;;
-	2)
-		echo "Part Time"
-		;;
-	*)
-		echo "Please enter the valid input"
-		;;
-	esac
-	
+fullTimeHour=8
+wagePerHour=20
+#calculate the daily wage per hour for full time employee employee
+fullTimeEmployeeWage()
+{
+	echo "Full time Wage for employee is:$((fullTimeHour*wagePerHour)) per day"
+}
+fullTimeEmployeeWage
+
+partTimeHour=4
+wagePerHours=20
+#calculate the daily wage per hour for full time employee employee
+partTimeEmployeeWage()
+{
+	echo "Full time Wage for employee is:$((partTimeHour*wagePerHours)) per day"
+}
+partTimeEmployeeWage
