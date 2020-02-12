8ed2f2973fc245ef315c3cf468f1515e1f86bcb4
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-12 18:29:54 +0530

    [Mayank] Refactor. added part time wage functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,9 @@
 #!bin/bash
-echo   "============================================"
-echo   "welcome To Employee Wage Computation Program"
-echo   "============================================"
+partTimeHour=4
+wagePerHours=20
+#calculate the daily wage per hour for full time employee employee
+partTimeEmployeeWage()
+{
+	echo "Full time Wage for employee is:$((partTimeHour*wagePerHours)) per day"
+}
+partTimeEmployeeWage
