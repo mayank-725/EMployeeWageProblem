c6f1854480d871a78909568c427ca2585c57d995
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-13 09:48:36 +0530

    [Mayank] Refactor. adding switch case calling logic for job type

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,16 @@
 #!bin/bash
-echo   "============================================"
-echo   "welcome To Employee Wage Computation Program"
-echo   "============================================"
+echo -e "Please select the options:\n1.Full time\n2.PartTime"
+read jobType
+
+case $jobType in
+    1)
+		echo "Full time"
+		;;
+	2)
+		echo "Part Time"
+		;;
+	*)
+		echo "Please enter the valid input"
+		;;
+	esac
+	
