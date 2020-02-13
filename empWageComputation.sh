9a6603f51e5feadc217de0e6d9106fd327d428ef
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-13 16:09:40 +0530

    [Mayank] Refactor. modifeid file and added return type

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,10 +5,12 @@ attendanceCheck()
 	if(($((RANDOM%2==0))))
     	then
 		attendanceResult=0
-		echo "Employee is absent today"
+		#echo "Employee is absent today"
+		return $attendanceResult
 	else
 		attendanceResult=1
-		echo "Employee is present today"
+		#echo "Employee is present today"
+		return $attendanceResult
 	fi
 }
 attendanceCheck
