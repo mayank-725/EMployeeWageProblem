1786c91473a6bc699a7a6e7bc97d718fbeca302b
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-13 11:20:05 +0530

    [Mayank] Refactor. calculate monthly wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,22 @@
 #!bin/bash
-echo   "============================================"
-echo   "welcome To Employee Wage Computation Program"
-echo   "============================================"
+workDay=0
+counter=0
+monthWage=0
+calculatMonthlyWage()
+{
+	while ((counter!=0))
+	do
+		attendanceCheck
+		result=$?
+		if((result==1)) 
+		then
+			workday=$((workday+1))
+			fullTimeEmployeeWage
+			res=$?
+			monthWage=res+monthWage
+		fi 	
+			counter=$((counter+1))	
+	done	
+	echo "monthly wage of employee is:$monthWage"
+	return $monthWage
+}
