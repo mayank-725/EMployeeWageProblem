b66907b0dc2ac436a475f0f23537dc1cacbfa9cf
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-14 10:57:52 +0530

    [Mayank] Refactor. storing wage day by day

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,37 @@
 #!bin/bash
-echo   "============================================"
-echo   "welcome To Employee Wage Computation Program"
-echo   "============================================"
+declare -a dailyWage
+checAttendance(){
+	if(( $((RANDOM%2))==0 ))
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+totalWages=0
+count=0
+
+getWorkingHours(){
+	wage=$1
+	while((count!=20))
+	do
+		checAttendance
+		result=$?
+		if(($result==1))
+		then
+			dailyWage[$count]=$wage
+		else
+			dailyWage[$count]=0
+		fi
+		count=$((count+1))
+	done
+}
+
+getWorkingHours 160
+
+echo "Daily Wage"
+for((i=0;i<20;i++))
+do
+	printf  "Day %02d: ${dailyWage[$i]}\n" "$((i+1))"
+done
