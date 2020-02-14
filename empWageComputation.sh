1fe2cada2cb776d996ad7e7f6b8473d3a9d38b84
mayank.sachdeva725@gmail.com <mayank.sachdeva725@gmail.com>
2020-02-14 10:11:21 +0530

    [Mayank] Refactor. adding daily wage store functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,37 @@
 #!bin/bash
-echo   "============================================"
-echo   "welcome To Employee Wage Computation Program"
-echo   "============================================"
+declare -a dailyWage
+Attendance(){
+	if(( $((RANDOM%2))==0 ))
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+totalWages=0
+count=0
+
+WorkingHours(){
+	wage=$1
+	while((count!=20))
+	do
+		Attendance
+		result=$?
+		if(($result==1))
+		then
+			dailyWage[$count]=$wage
+		else
+			dailyWage[$count]=0
+		fi
+		count=$((count+1))
+	done
+}
+
+WorkingHours 160
+
+echo "Daily Wage"
+for((i=0;i<20;i++))
+do
+	echo -n "${dailyWage[$i]} "
+done
